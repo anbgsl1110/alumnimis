bd28fea7d92b6c52e78a2ac01bc2d9a17883935e
aliang <anbgsl1110@gmail.com>
2017-05-16 18:35:43 +0800

    数据库数据初始化

--- AlumniMis/AlumniMis.Data/DataBase/InitData.cs
+++ AlumniMis/AlumniMis.Data/DataBase/InitData.cs
@@ -31,11 +31,67 @@ namespace AlumniMis.Data.DataBase
                     Aname = "活动" + i,
                     Atag = "演示",
                     Amaster = "周树琴",
-                    Atel = "12345678",
+                    Atel = "18294567867",
                     Apub = DateTime.Now,
                     Astart = DateTime.Now,
                     Apot = "杭电",
                     Aspec = "测试"
+                });
+                dbContext.AdminInfos.Add(new AdminInfo
+                {
+                    Name = "管理员" + i,
+                    Phone = "18294567867"
+                });
+                dbContext.AlumniInfos.Add(new AlumniInfo
+                {
+                    Anum = "AlumniInfo" + numberTemp + i,
+                    Aname =  "校友" + 1,
+                    Asex = "女",
+                    Atel = "18294567867",
+                    Aclass = "三二班",
+                    Acoll = "管理学院",
+                    Aenter = DateTime.Now.AddDays(-1110),
+                    Aleave = DateTime.Now.AddDays(-520),
+                    Addres = "浙江杭州",
+                    Acompany = "杭电有限公司",
+                    Ajob = "CTO"
+                });
+                dbContext.AlumniOrgans.Add(new AlumniOrgan
+                {
+                    Gnum = "AlumniOrgan" + numberTemp + i,
+                    Gname = "组织" + i,
+                    Gtag = "教育",
+                    GMaster = "master",
+                    Gtel = "18294567867"
+                });
+                dbContext.Donates.Add(new Donate
+                {
+                    Dnum = "Donate" + numberTemp + i,
+                    Dname = "捐赠" + i,
+                    Dtag = "hhh",
+                    Dpub = DateTime.Now,
+                    Dpuber = "发起人",
+                    Dres = "负责人",
+                    Dtel = "18294567867",
+                    Dstatus = 0,
+                    Dremarks = "捐赠备注"
+                });
+                dbContext.NewReleases.Add(new NewRelease
+                {
+                    Nnum = "NewRelease" + numberTemp + i,
+                    Ntit = "新闻" + i,
+                    Nauthor = "作者",
+                    Ntime = DateTime.Now,
+                    Ntag = "通知",
+                    Ncontent = "怎么过一天，就怎么过一生"
+                });
+                dbContext.RequestServices.Add(new RequestService
+                {
+                    Snum = "RequestService" + numberTemp + i,
+                    Sname = "服务"+ i,
+                    Sres = "负责人",
+                    Stel = "18294567867",
+                    Status = false
                 });
             }
         }
