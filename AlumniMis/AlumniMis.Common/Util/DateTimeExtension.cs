0461301fb4681528b83b10144d3e4f0b494f909d
aliang <anbgsl1110@gmail.com>
2017-05-17 21:50:44 +0800

    首页新闻和详情初步联调

---
+++ AlumniMis/AlumniMis.Common/Util/DateTimeExtension.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AlumniMis.Common.Util
+{
+    /// <summary>
+    /// 时间扩展方法
+    /// </summary>
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime ToStartTime(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
+        }
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime ToEndTime(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 23, 59, 59);
+        }
+        /// <summary>
+        /// 下一天开始时间
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime ToNextDayStartTime(this DateTime date)
+        {
+            date = date.AddDays(1);
+            return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
+        }
+        /// <summary>
+        /// 上一天结束时间
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime ToLastDayEndTime(this DateTime date)
+        {
+            date = date.AddDays(-1);
+            return new DateTime(date.Year, date.Month, date.Day, 23, 59, 59);
+        }
+        /// <summary>
+        /// DateTime转时间戳
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static long ToTimeStamp(this DateTime date)
+        {
+            DateTime dateStart = new DateTime(1970, 1, 1, 0, 0, 0);
+            return Convert.ToInt64((date.ToUniversalTime() - dateStart).TotalMilliseconds);
+        } 
+    }
+}
---
+++ AlumniMis/AlumniMis.Web/Controllers/NewsController.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Linq;
+using System.Web.Mvc;
+using AlumniMis.Data.DataTable;
+using AlumniMis.Services.Service.Service;
+
+namespace AlumniMis.Web.Controllers
+{
+    public class NewsController : Controller
+    {
+        /// <summary>
+        /// 首页(新闻)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult HomePage()
+        {
+            NewReleaseService service = new NewReleaseService();
+            var newsListResult = service.Select(new NewRelease(),0,1000).Data.ToList();
+            var schoolNews = newsListResult.Where(p => p.Ntype.Equals("校内新闻")).Take(5).ToList();
+            ViewBag.SchoolNews = schoolNews;
+            var alumniNews = newsListResult.Where(p => p.Ntype.Equals("校友新闻")).Take(5).ToList();
+            ViewBag.AlumniNews = alumniNews;
+            return View();
+        }
+
+        /// <summary>
+        /// 新闻详情
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NewsDetail(long id = 3)
+        {
+            NewReleaseService service = new NewReleaseService();
+            var newsResult = service.Select(new NewRelease(), id).Data;
+            if (newsResult == null)
+            {
+                return Redirect("/Error");
+            }
+            ViewBag.News = newsResult;
+            return View();
+        }
+    }
+}
