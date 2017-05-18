bef3a864134cb59532ad39a7dbc9d237111d8723
aliang <anbgsl1110@gmail.com>
2017-05-18 21:15:46 +0800

    校友天地雏形

---
+++ AlumniMis/AlumniMis.Web/Controllers/ActivityController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace AlumniMis.Web.Controllers
+{
+    public class ActivityController : Controller
+    {
+        /// <summary>
+        /// 活动列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ActivityList()
+        {
+            return View();
+        }
+    }
+}
